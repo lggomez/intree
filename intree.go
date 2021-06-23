43969c79f2c614afb98529550ab9fb1c28465df7
Luis Gabriel Gomez <luis.gg1990@gmail.com>
2021-06-23 17:30:05 -0300

    Improve package code readability, add comments

--- intree.go
+++ intree.go
@@ -1,6 +1,6 @@
 // MIT License
 //
-// Copyright (c) 2020 geozelot (André Siefken)
+// Copyright (c) 2020 geozelot (André Siefken), 2021 Luis Gomez
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -19,6 +19,8 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+//
+// Changelog: Improve package code readability, add comments
 
 // Package intree provides a very fast, static, flat, augmented interval tree for reverse range searches.
 package intree
@@ -30,156 +32,137 @@ import (
 
 // Bounds is the main interface expected by NewINTree(); requires Limits method to access interval limits.
 type Bounds interface {
-	Limits() (Lower, Upper float64)
+	Limits() (lower, upper float64)
 }
 
 // INTree is the main package object;
 // holds Slice of reference indices and the respective interval limits.
 type INTree struct {
-	idxs []int
-	lmts []float64
+	indexes []int
+	limits  []float64
 }
 
 // buildTree is the internal tree construction function;
 // creates, sorts and augments nodes into Slices.
-func (inT *INTree) buildTree(bnds []Bounds) {
+func (t *INTree) buildTree(bounds []Bounds) {
+	t.indexes = make([]int, len(bounds))
+	t.limits = make([]float64, 3*len(bounds))
 
-	inT.idxs = make([]int, len(bnds))
-	inT.lmts = make([]float64, 3*len(bnds))
-
-	for i, v := range bnds {
-
-		inT.idxs[i] = i
+	for i, v := range bounds {
+		t.indexes[i] = i
 		l, u := v.Limits()
 
-		inT.lmts[3*i] = l
-		inT.lmts[3*i+1] = u
-		inT.lmts[3*i+2] = 0
-
+		t.limits[3*i] = l
+		t.limits[3*i+1] = u
+		t.limits[3*i+2] = 0
 	}
 
-	sort(inT.lmts, inT.idxs)
-	augment(inT.lmts, inT.idxs)
-
+	sort(t.limits, t.indexes)
+	augment(t.limits, t.indexes)
 }
 
 // Including is the main entry point for bounds searches;
 // traverses the tree and collects intervals that overlap with the given value.
-func (inT *INTree) Including(val float64) []int {
+func (t *INTree) Including(val float64) []int {
+	idxStock := []int{0, len(t.indexes) - 1}
+	result := []int{}
 
-	stk := []int{0, len(inT.idxs) - 1}
-	res := []int{}
+	for len(idxStock) > 0 {
+		// Retrieve right and left boundaries from index stock
+		rBoundIdx := idxStock[len(idxStock)-1]
+		idxStock = idxStock[:len(idxStock)-1]
+		lBoundIdx := idxStock[len(idxStock)-1]
+		idxStock = idxStock[:len(idxStock)-1]
 
-	for len(stk) > 0 {
-
-		rb := stk[len(stk)-1]
-		stk = stk[:len(stk)-1]
-		lb := stk[len(stk)-1]
-		stk = stk[:len(stk)-1]
-
-		if lb == rb+1 {
+		if lBoundIdx == rBoundIdx+1 {
 			continue
 		}
 
-		cn := int(math.Ceil(float64(lb+rb) / 2.0))
-		nm := inT.lmts[3*cn+2]
+		centerIdx := int(math.Ceil(float64(lBoundIdx+rBoundIdx) / 2.0))
+		lowerLimit := t.limits[3*centerIdx+2]
 
-		if val <= nm {
-
-			stk = append(stk, lb)
-			stk = append(stk, cn-1)
-
+		if val <= lowerLimit {
+			idxStock = append(idxStock, lBoundIdx, centerIdx-1)
 		}
 
-		l := inT.lmts[3*cn]
+		l := t.limits[3*centerIdx]
 
 		if l <= val {
+			idxStock = append(idxStock, centerIdx+1, rBoundIdx)
 
-			stk = append(stk, cn+1)
-			stk = append(stk, rb)
+			upperLimit := t.limits[3*centerIdx+1]
 
-			u := inT.lmts[3*cn+1]
-
-			if val <= u {
-				res = append(res, inT.idxs[cn])
+			if val <= upperLimit {
+				result = append(result, t.indexes[centerIdx])
 			}
-
 		}
-
 	}
 
-	return res
-
+	return result
 }
 
 // NewINTree is the main initialization function;
 // creates the tree from the given Slice of Bounds.
-func NewINTree(bnds []Bounds) *INTree {
+func NewINTree(bounds []Bounds) *INTree {
+	tree := INTree{}
+	tree.buildTree(bounds)
 
-	inT := INTree{}
-	inT.buildTree(bnds)
-
-	return &inT
-
+	return &tree
 }
 
 // augment is an internal utility function, adding maximum value of all child nodes to the current node.
-func augment(lmts []float64, idxs []int) {
-
-	if len(idxs) < 1 {
+func augment(limits []float64, indexes []int) {
+	if len(indexes) < 1 {
 		return
 	}
 
 	max := 0.0
 
-	for idx := range idxs {
-
-		if lmts[3*idx+1] > max {
-			max = lmts[3*idx+1]
+	for idx := range indexes {
+		if limits[3*idx+1] > max {
+			max = limits[3*idx+1]
 		}
-
 	}
 
-	r := len(idxs) >> 1
+	r := len(indexes) >> 1
 
-	lmts[3*r+2] = max
+	limits[3*r+2] = max
 
-	augment(lmts[:3*r], idxs[:r])
-	augment(lmts[3*r+3:], idxs[r+1:])
-
+	augment(limits[:3*r], indexes[:r])
+	augment(limits[3*r+3:], indexes[r+1:])
 }
 
 // sort is an internal utility function, sorting the tree by lowest limits using Random Pivot QuickSearch
-func sort(lmts []float64, idxs []int) {
-
-	if len(idxs) < 2 {
+func sort(limits []float64, indexes []int) {
+	if len(indexes) < 2 {
 		return
 	}
 
-	l, r := 0, len(idxs)-1
+	// Pick index bounds
+	l, r := 0, len(indexes)-1
 
-	p := rand.Int() % len(idxs)
+	// Pick pivot
+	p := rand.Int() % len(indexes)
 
-	idxs[p], idxs[r] = idxs[r], idxs[p]
-	lmts[3*p], lmts[3*p+1], lmts[3*p+2], lmts[3*r], lmts[3*r+1], lmts[3*r+2] = lmts[3*r], lmts[3*r+1], lmts[3*r+2], lmts[3*p], lmts[3*p+1], lmts[3*p+2]
+	// Perform in-place assignment of limits and indexes
+	indexes[p], indexes[r] = indexes[r], indexes[p]
+	limits[3*p], limits[3*p+1], limits[3*p+2], limits[3*r], limits[3*r+1], limits[3*r+2] = limits[3*r], limits[3*r+1], limits[3*r+2], limits[3*p], limits[3*p+1], limits[3*p+2]
 
-	for i := range idxs {
-
-		if lmts[3*i] < lmts[3*r] {
-
-			idxs[l], idxs[i] = idxs[i], idxs[l]
-			lmts[3*l], lmts[3*l+1], lmts[3*l+2], lmts[3*i], lmts[3*i+1], lmts[3*i+2] = lmts[3*i], lmts[3*i+1], lmts[3*i+2], lmts[3*l], lmts[3*l+1], lmts[3*l+2]
+	for i := range indexes {
+		if limits[3*i] < limits[3*r] {
+			// Perform in-place assignment of limits and indexes
+			indexes[l], indexes[i] = indexes[i], indexes[l]
+			limits[3*l], limits[3*l+1], limits[3*l+2], limits[3*i], limits[3*i+1], limits[3*i+2] = limits[3*i], limits[3*i+1], limits[3*i+2], limits[3*l], limits[3*l+1], limits[3*l+2]
 
 			l++
-
 		}
-
 	}
 
-	idxs[l], idxs[r] = idxs[r], idxs[l]
-	lmts[3*l], lmts[3*l+1], lmts[3*l+2], lmts[3*r], lmts[3*r+1], lmts[3*r+2] = lmts[3*r], lmts[3*r+1], lmts[3*r+2], lmts[3*l], lmts[3*l+1], lmts[3*l+2]
+	// Perform in-place assignment of limits and indexes
+	indexes[l], indexes[r] = indexes[r], indexes[l]
+	limits[3*l], limits[3*l+1], limits[3*l+2], limits[3*r], limits[3*r+1], limits[3*r+2] = limits[3*r], limits[3*r+1], limits[3*r+2], limits[3*l], limits[3*l+1], limits[3*l+2]
 
-	sort(lmts[:3*l], idxs[:l])
-	sort(lmts[3*l+3:], idxs[l+1:])
-
+	// Tail recursive calls on branches
+	sort(limits[:3*l], indexes[:l])
+	sort(limits[3*l+3:], indexes[l+1:])
 }
